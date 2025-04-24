61f2e95857da08705830c36fab3a407dd68bd384
Kauã Felipe Alves <kauafelipeoficial191@gmail.com>
2025-04-24 11:33:11 -0300

    Resolvendo problema de documentação da api.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const options = {
     },
     servers: [
       {
-        url: `http://localhost:${3000}`, 
+        url: `https://api-sistema-educacional.vercel.app/api`, 
       },
     ],
     components: {
