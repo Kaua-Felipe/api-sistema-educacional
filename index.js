2f995aa800d63a48ed91ea15d52857432a23c37b
Kauã Felipe Alves <kauafelipeoficial191@gmail.com>
2025-05-09 11:44:35 -0300

    Resolução de produção 6

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ app.get('/api-docs', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000
+/*const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
-})
+})*/
 
 export default app
