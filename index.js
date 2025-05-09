e52cfbb593507fdfeba3342b0ae9954e35483f5a
Kauã Felipe Alves <kauafelipeoficial191@gmail.com>
2025-05-09 11:43:21 -0300

    Resolução de produção 6

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import swaggerJSDoc from 'swagger-jsdoc'
-import swaggerUi from 'swagger-ui-express'
-
-import serverless from 'serverless-http';
+import fs from "fs"
+import path from 'path'
+import { fileURLToPath } from 'url'
 
 import professorsRoutes from "./routes/professores.routes.js"
 import instituicoesRoutes from "./routes/instituicoes.routes.js"
@@ -15,117 +14,10 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Sistema educacional',
-      version: '1.0.0',
-      description: 'Documentação automática da API Express.',
-    },
-    servers: [
-      {
-        url: 'https://api-sistema-educacional.vercel.app/api', 
-      },
-    ],
-    components: {
-      schemas: {
-        Professor: {
-          type: 'object',
-          properties: {
-            id: {
-              type: 'integer',
-              format: 'int64',
-              description: 'ID do professor.',
-              readOnly: true,
-            },
-            name: {
-              type: 'string',
-              description: 'Nome do professor.',
-            },
-            email: {
-              type: 'string',
-              format: 'email',
-              description: 'Email do professor.',
-            },
-            password: {
-              type: 'string',
-              description: 'Senha do professor (normalmente não retornada em GETs).',
-            },
-          },
-        }, 
-        Instituição: {
-          type: 'object', 
-          properties: {
-            id: {
-              type: 'integer',
-              format: 'int64',
-              description: 'ID da instituição.',
-              readOnly: true,
-            },
-            name: {
-              type: 'string',
-              description: 'Nome da instituição.',
-            },
-            cnpj: {
-              type: 'string',
-              description: 'Cnpj da instituição.',
-            },
-            cep: {
-              type: 'string',
-              description: 'Cep da instituição.',
-            },
-            address_number: {
-              type: 'string',
-              description: 'Número de endereço da instituição.',
-            },
-            id_professor: {
-              type: 'integer',
-              format: 'int64',
-              description: 'ID referenciando o professor que cadastrou a instituição.',
-            },
-          },
-        }, 
-        
-        Erro: {
-          type: 'object',
-          properties: {
-            error: {
-              type: 'string',
-              description: 'Mensagem de erro.',
-            },
-          },
-        },
-      },
-    },
-  },
-  apis: [
-    './routes/professores.routes.js', 
-    './routes/instituicoes.routes.js', 
-    './routes/turmas.routes.js', 
-    './routes/alunos.routes.js', 
-    './app.js'
-  ], 
-};
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-const swaggerSpec = swaggerJSDoc(options)
-/*app.use('/api-docs', swaggerUi.serve);
-app.get('/api-docs', (req, res) => {
-  res.send(
-    swaggerUi.generateHTML(swaggerSpec, {
-      explorer: true,
-      swaggerOptions: {
-        url: '/api-docs/swagger.json',
-      },
-    })
-  );
-});
-app.get('/api-docs/swagger.json', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(swaggerSpec);
-});*/
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 // Rotas
 app.use("/api/v1/professores", professorsRoutes)
@@ -137,11 +29,22 @@ app.get('/', (req, res) => {
   res.send('API funcionando com import/export!');
 })
 
-/*if(process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 3000
-  app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-  })
-}*/
+app.get('/api-docs', (req, res) => {
+  const swaggerPath = path.join(__dirname, '/swagger/swagger.json');
+  console.log('Caminho do arquivo Swagger:', swaggerPath); // Adicione esta linha
+  fs.readFile(swaggerPath, 'utf8', (err, data) => {
+      if (err) {
+          console.error('Erro ao ler o arquivo Swagger:', err); // Adicione esta linha
+          return res.status(500).send('Erro ao carregar o Swagger');
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.send(data);
+  });
+});
 
-export const handler = serverless(app)
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+})
+
+export default app
