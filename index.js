8b524a375e601e2f17aa258efc458f721b3d6104
Kauã Felipe Alves <kauafelipeoficial191@gmail.com>
2025-05-12 18:02:18 -0300

    Finalizando erros de api.

--- index.js
+++ index.js
@@ -119,9 +119,9 @@ app.get('/', (req, res) => {
   res.send('API funcionando com import/export!');
 })
 
-const PORT = process.env.PORT || 3000
+/*const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
-})
+})*/
 
 export default app
